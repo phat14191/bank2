7e2c9ab485f6dc9fb1a786e5527bf838ab9d5196
Charles <charles.emrich@gmail.com>
2017-05-09 09:12:13 -0700

    add js to pass first spec: movie title list populates based on backend list

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ When a movie is selected, a user should be able to select showtimes.
 When a showtime is selected, they should be able to see prices.
 
 write a function to parse a movie's status based on time + second-run
+
+Located in close-SE Portland off of 20th & Hawthorne, this is an independent single-screen theater that shows first-run movies, second-run films and classics.
+All showings before 6pm are Matinees = $5.00 per person
+All showings after 6pm are General Admission = $7.00 per person
+Children under 11, Seniors 62 and over, Military and Student with ID are all $5.00 per person
+Tuesday screenings are at matinee prices all day long
+
+Specs
+Upon page load: populate list of movies depending on list from backend.
+User can click a movie in the populated list to see showtimes.
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 /// Business Logic ///
-var
+var moviesBackend = [
+  ["Guardians of the Galaxy Vol. 2 3D",["11:15AM","2:30PM","6:00PM","10:20PM"], "first", true],
+  ["Guardians of the Galaxy Vol. 2",["12:00PM","12:45PM","3:30PM","4:30PM","7:00PM","8:00PM","9:30PM"], "first", false],
+  ["The Circle", ["11:00AM","1:45PM","4:15PM","7:15PM"], "first", false],
+  ["The Fate of the Furious 3D",["12:15PM","3:45PM","7:00PM"], "first", true],
+  ["The Fate of the Furious",["12:30PM","3:15PM","10:15PM"], "first", false],
+  ["Get Out", ["4:45PM","7:00PM","9:15PM"], "second", false]
+];
 
 function Movie(title, showtimes, run, threeD) {
   this.title     = title;
@@ -8,16 +15,23 @@ function Movie(title, showtimes, run, threeD) {
   this.threeD    = threeD;
 }
 
-function Ticket(title, time) {
+function Ticket(title, time, age) {
   this.title = title;
   this.time  = time;
+  this.age   = age;
 }
 
 Ticket.prototype.price = function () {
 
 };
 
+function parseTime() {
+
+}
+
 /// User Interface Logic ///
 $(function() {
-
+  for (var i = 0; i < moviesBackend.length; i++) {
+    $("ul#movie-list").append("<li>" + moviesBackend[i][0] + "</li>");
+  }
 });
